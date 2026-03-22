95f377bd064a3f4db9ed045b8a03a670fc6611c3
rhemat23-cloud <rhemat23@gmail.com>
2026-03-22 21:43:54 +0530

    Add files via upload

---
+++ task10.py
@@ -0,0 +1,13 @@
+# Demonstrating Python logical operators
+
+# Expression 1: AND operator
+# True AND True → True
+print(10 > 5 and 5 == 5)  
+
+# Expression 2: OR operator
+# False OR True → True
+print(5 > 10 or 10 == 10)  
+
+# Expression 3: NOT operator
+# NOT(True) → False
+print(not (5 > 2))
