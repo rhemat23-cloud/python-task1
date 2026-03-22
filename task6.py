3d132be15c6251db4ff07ee22f661cda3847f605
rhemat23-cloud <rhemat23@gmail.com>
2026-03-22 21:17:13 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,7 @@
+# Task 6 – BODMAS Expression
+# Ask the team to predict the answer first
+print("Predict the result of: 3 + 2 * 5 ** 2")
+
+# Now run the code to check
+result = 3 + 2 * 5 ** 2
+print("Actual result:", result)
