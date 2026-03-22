21367ad9020425a94fc584408692c8a06f23cbc3
rhemat23-cloud <rhemat23@gmail.com>
2026-03-22 21:36:12 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,13 @@
+# Assignment Operator
+
+# First example: += operator
+num = 50
+num += 25  # Adds 25 to num
+print("After num += 25, num =", num)
+
+# Second example: /= operator
+num = 100
+num /= 10  # Divides num by 10
+print("After num /= 10, num =", num)
+
+
