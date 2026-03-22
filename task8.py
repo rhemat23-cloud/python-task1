da5d6c47e01c6a0d46138b60b68349234c449c9a
rhemat23-cloud <rhemat23@gmail.com>
2026-03-22 21:39:03 +0530

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,20 @@
+# Comparison expressions
+expr1 = 10 > 5        # Greater than
+expr2 = 20 < 15       # Less than
+expr3 = 5 == 5        # Equal to
+expr4 = 10 != 8       # Not equal to
+expr5 = 7 >= 7        # Greater than or equal to
+
+# Print the results
+print("10 > 5:", expr1)
+print("20 < 15:", expr2)
+print("5 == 5:", expr3)
+print("10 != 8:", expr4)
+print("7 >= 7:", expr5)
+
+# Usage Example Output
+# 10 > 5: True
+# 20 < 15: False
+# 5 == 5: True
+# 10 != 8: True
+# 7 >= 7: True
